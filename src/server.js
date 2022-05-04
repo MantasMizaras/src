c9456c1b412b8a42190dd7082f27070fae1d6741
GediminasKul <gediminas.kulakauskas@gmail.com>
2022-05-04 11:18:33 +0300

    path changes, cors, morgan and mysql added.

--- src/server.js
+++ src/server.js
@@ -1,5 +1,9 @@
 const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+// const mysql = require('mysql2/promise');
 const app = express();
 const { PORT } = require('./config');
 
+app.use(morgan('dev'));
 app.listen(PORT, () => console.log('server is running'));
